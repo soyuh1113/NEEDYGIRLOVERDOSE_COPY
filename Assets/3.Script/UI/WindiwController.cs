9cea5f2642074193d2d8082e7ea0f116bfe7e225
soyuh1113 <121688618+soyuh1113@users.noreply.github.com>
2024-08-29 13:31:24 +0900

    Option UI
    옵션 UI 제작 중

---
+++ Assets/3.Script/UI/WindiwController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindiwController : MonoBehaviour
+{
+    public void close_btn()
+    {
+        Debug.Log("정상 작동");
+        
+    }
+}
