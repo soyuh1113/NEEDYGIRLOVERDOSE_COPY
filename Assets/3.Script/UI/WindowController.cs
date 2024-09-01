84580de14d2c5f1ee5ce504b73a9ed90e857d769
soyuh1113 <soyuh1113@gmail.com>
2024-09-02 03:36:23 +0900

    UI Drag
    UI 드래그 기능 추가

--- Assets/3.Script/UI/WindowController.cs
+++ Assets/3.Script/UI/WindowController.cs
@@ -9,7 +9,7 @@ public class WindowController : MonoBehaviour, IPointerDownHandler, IDragHandler
     [SerializeField] private Transform moveUITarget;
     [SerializeField] private RectTransform uiTarget;
 
-    private Vector2 originalSize;
+    //private Vector2 originalSize;
     private Vector2 originPos;
     private Vector2 originMousePos;
     private Vector2 movedPos;
@@ -26,7 +26,7 @@ public class WindowController : MonoBehaviour, IPointerDownHandler, IDragHandler
         cursorLockMode = Cursor.lockState;
         Cursor.lockState = CursorLockMode.Confined;
 
-        originalSize = uiTarget.sizeDelta;
+       // originalSize = uiTarget.sizeDelta;
         originPos = moveUITarget.position;
         originMousePos = eventData.position;
 
@@ -36,12 +36,35 @@ public class WindowController : MonoBehaviour, IPointerDownHandler, IDragHandler
     public void OnDrag(PointerEventData eventData)
     {
         movedPos = eventData.position - originMousePos;
-        moveUITarget.position = originPos + movedPos;
+        Vector2 newPosition = originPos + movedPos;
+
+        RectTransform parentRectTransform = uiTarget.parent.GetComponent<RectTransform>();
+        RectTransform uiRectTransform = uiTarget.GetComponent<RectTransform>();
+
+        Vector3[] parentCorners = new Vector3[4];
+        parentRectTransform.GetWorldCorners(parentCorners);
+
+        Vector3[] uiCorners = new Vector3[4];
+        uiRectTransform.GetWorldCorners(uiCorners);
+
+        float minX = parentCorners[0].x + (uiRectTransform.rect.width * uiTarget.lossyScale.x) / 2;
+        float maxX = parentCorners[2].x - (uiRectTransform.rect.width * uiTarget.lossyScale.x) / 2;
+
+        float minY = parentCorners[0].y + (uiRectTransform.rect.height * uiTarget.lossyScale.y) / 2;
+        float maxY = parentCorners[2].y - (uiRectTransform.rect.height * uiTarget.lossyScale.y) / 2;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+            
+        moveUITarget.position = newPosition;
 
         if (Input.GetMouseButtonUp(0) || !gameObject.activeInHierarchy)
+        {
             StopDrag();
+        }
+            
 
-        Vector2 mouseDelta = eventData.position - originMousePos;
-        uiTarget.sizeDelta = new Vector2(originalSize.x + mouseDelta.x, originalSize.y + mouseDelta.y);
+       // Vector2 mouseDelta = eventData.position - originMousePos;
+       // uiTarget.sizeDelta = new Vector2(originalSize.x + mouseDelta.x, originalSize.y + mouseDelta.y);
     }
 }
