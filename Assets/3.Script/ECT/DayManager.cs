de7fd8203d07ba35f541ed8de07d712bfad6d989
soyuh1113 <soyuh1113@gmail.com>
2024-09-03 08:19:43 +0900

    Day Manager
    스크립트 내용 추가

--- Assets/3.Script/ECT/DayManager.cs
+++ Assets/3.Script/ECT/DayManager.cs
@@ -6,7 +6,7 @@ using TMPro;
 public enum DayType
 {
     Afternoon,
-    Dinner,
+    Evening,
     Night
 }
 
@@ -14,8 +14,73 @@ public class DayManager : MonoBehaviour
 {
     public DayType daytype;
 
+    private int totalDay = 30;
+    private int currentDay = 1;
+
     [SerializeField] private TextMeshProUGUI today;
     [SerializeField] private TextMeshProUGUI nextday;
 
+    void Start()
+    {
+        StartCoroutine(DayCycle());
+    }
 
+    IEnumerator DayCycle()
+    {
+        while (currentDay <= totalDay)
+        {
+            yield return StartCoroutine(StartNewDay());
+
+            yield return StartCoroutine(PerformDayActivity());
+            yield return StartCoroutine(PerformEveningActivity());
+            yield return StartCoroutine(PerformNightActivity());
+
+            currentDay++;
+        }
+
+        Debug.Log("All days completed.");
+    }
+
+    IEnumerator StartNewDay()
+    {
+        Debug.Log("Day " + currentDay + " starts.");
+
+        daytype = DayType.Afternoon;
+        Debug.Log("Current period: " + daytype);
+
+        yield return null;
+    }
+
+    IEnumerator PerformDayActivity()
+    {
+        Debug.Log("Performing day activity...");
+
+        yield return new WaitForSeconds(2);
+
+        Debug.Log("Day activity completed.");
+
+        daytype = DayType.Evening;
+        Debug.Log("Current period: " + daytype);
+    }
+
+    IEnumerator PerformEveningActivity()
+    {
+        Debug.Log("Performing evening activity...");
+
+        yield return new WaitForSeconds(2);
+
+        Debug.Log("Evening activity completed.");
+
+        daytype = DayType.Night;
+        Debug.Log("Current period: " + daytype);
+    }
+
+    IEnumerator PerformNightActivity()
+    {
+        Debug.Log("Performing night activity...");
+
+        yield return new WaitForSeconds(2);
+
+        Debug.Log("Night activity completed.");
+    }
 }
