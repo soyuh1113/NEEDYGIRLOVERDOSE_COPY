aeae61435f98f2d228888ef1b731f3078767ff60
soyuh1113 <121688618+soyuh1113@users.noreply.github.com>
2024-09-04 15:05:24 +0900

    UI
    UI 수정

--- Assets/3.Script/UI/CursorController.cs
+++ Assets/3.Script/UI/CursorController.cs
@@ -5,15 +5,19 @@ using UnityEngine.EventSystems;
 
 public class CursorController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Texture2D Change_Cursor;
+    public Texture2D Change_Cursor1;
+
+    public bool Change_Cursor = false;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Cursor.SetCursor(Change_Cursor, Vector2.zero, CursorMode.ForceSoftware);
+        Change_Cursor = true;
+        Cursor.SetCursor(Change_Cursor1, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        Change_Cursor = false;
         Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
     }
 }
